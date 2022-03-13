edeb40b1f4148090e41550b4c88196d038027db7
RosicaNikolova <rosicanikolova1402@gmail.com>
2022-03-13 13:42:04 +0100

    connected to a database

--- RestaurantReviews/RestaurantReviews/Models/UserRepository.cs
+++ RestaurantReviews/RestaurantReviews/Models/UserRepository.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace RestaurantReviews.Models
 {
     public class UserRepository
     {
+
+        public User FindUser(string username, string password)
+        {
+      
+            return null;
+        }
+
+        public void SaveUser(User user)
+        {
+
+        }
+      
+        public void DeleteUser(User user)
+        {
+
+        }
     }
 }
