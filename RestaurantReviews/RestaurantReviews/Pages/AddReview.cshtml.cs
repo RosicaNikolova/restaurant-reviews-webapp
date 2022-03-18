a8242e4d433f1b630a10932d377fadf1f6aa9d28
RosicaNikolova <rosicanikolova1402@gmail.com>
2022-03-18 22:21:08 +0100

    added more exceptions and changes to design

---
+++ RestaurantReviews/RestaurantReviews/Pages/AddReview.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RestaurantReviews.Pages
+{
+    public class AddReviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
